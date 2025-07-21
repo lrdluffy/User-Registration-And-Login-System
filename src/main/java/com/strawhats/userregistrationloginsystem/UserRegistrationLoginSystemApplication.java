dca40a48d9dd2e6c38193dfe23c5a2a1f96d8b7f
lrdluffy <lrdaftkhar@gmail.com>
2025-07-21 11:36:33 +0330

    Project setup

---
+++ src/main/java/com/strawhats/userregistrationloginsystem/UserRegistrationLoginSystemApplication.java
@@ -0,0 +1,13 @@
+package com.strawhats.userregistrationloginsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationLoginSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationLoginSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/strawhats/userregistrationloginsystem/UserRegistrationLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strawhats.userregistrationloginsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationLoginSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
